87042459746bbc4d82d7a53c9b38e36d62f71614
Martina Magnusson <magnusson.martina@hotmail.com>
2015-12-21 14:04:02 +0100

    Logik för exercise 5

---
+++ Lab/Lab1/Script/Exercise5.js
@@ -0,0 +1,18 @@
+﻿var svar = document.getElementById('svar').addEventListener('click', skrivUtSvar, false);
+
+function skrivUtSvar() {
+    var maxTal = document.getElementById('hundra').innerHTML;
+    document.getElementById('svar').innerHTML = jämnaTal(maxTal);
+}
+
+function jämnaTal(maxTal) {
+    var tal = '';
+    for (var i = 1; i <= maxTal; i++) {
+        if (i % 2 == 0)
+            if (i != 100)
+                tal += String(i + ', ');
+            else 
+                tal += String(i);
+    }
+    return tal;
+}
