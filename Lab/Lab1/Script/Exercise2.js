a447e32fbf5bde01967865b85a7fa34e016cac91
Martina Magnusson <magnusson.martina@hotmail.com>
2015-12-21 11:45:00 +0100

    Html-koden för exercise 3

--- Lab/Lab1/Script/Exercise2.js
+++ Lab/Lab1/Script/Exercise2.js
@@ -1,17 +1,18 @@
-﻿var medelvärde = document.getElementById('medelvärde').addEventListener('click', medelvärdet, false);
-var summa = document.getElementById('summa').addEventListener('click', summan, false);
-function medelvärdet() {
+﻿var medelvärde = document.getElementById('medelvärde').addEventListener('click', skriverUtMedelvärdet, false);
+var summa = document.getElementById('summa').addEventListener('click', skrivUtSumman, false);
+
+function skriverUtMedelvärdet() {
     var tal1 = document.getElementById('tal1').value;
     var tal2 = document.getElementById('tal2').value;
     var tal3 = document.getElementById('tal3').value;
-    document.getElementById('medelvärde').innerHTML = räknaUtMedelvärdet(tal1, tal2, tal3);
+    document.getElementById('medelvärde').innerHTML = medelvärdet(tal1, tal2, tal3);
 }
 
-function räknaUtMedelvärdet(tal1,tal2,tal3) {
+function medelvärdet(tal1,tal2,tal3) {
     return (Number(tal1) + Number(tal2) + Number(tal3)) / 3;
 }
 
-function summan() {
+function skrivUtSumman() {
     var tal1 = document.getElementById('tal1').value;
     var tal2 = document.getElementById('tal2').value;
     var tal3 = document.getElementById('tal3').value;
