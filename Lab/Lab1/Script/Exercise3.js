6446a6d246a4b3c0c394b671cca745829ef2a26c
Martina Magnusson <magnusson.martina@hotmail.com>
2015-12-21 11:53:17 +0100

    lagt till logik för exercise 3, så att den räknar ut x*3+7

---
+++ Lab/Lab1/Script/Exercise3.js
@@ -0,0 +1,10 @@
+﻿var summa = document.getElementById('summa2').addEventListener('click', skrivUtSumman, false);
+
+function skrivUtSumman() {
+    var tal = document.getElementById('tal').value;
+    document.getElementById('summa2').innerHTML = räknaUtSumman(tal);
+}
+
+function räknaUtSumman(tal) {
+    return (Number(tal) * 3) + 7;
+}
