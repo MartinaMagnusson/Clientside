2d7aad2b6e2f333af4774864bb0fa8710e9da771
Martina Magnusson <magnusson.martina@hotmail.com>
2015-12-21 11:36:17 +0100

    lagt till logik för exercise 2 så att det skriver ut summan och medelvärdet av talen som skickas in

--- Lab/Lab1/Script/Exercise2.js
+++ Lab/Lab1/Script/Exercise2.js
@@ -1,1 +1,23 @@
-﻿
+﻿var medelvärde = document.getElementById('medelvärde').addEventListener('click', medelvärdet, false);
+var summa = document.getElementById('summa').addEventListener('click', summan, false);
+function medelvärdet() {
+    var tal1 = document.getElementById('tal1').value;
+    var tal2 = document.getElementById('tal2').value;
+    var tal3 = document.getElementById('tal3').value;
+    document.getElementById('medelvärde').innerHTML = räknaUtMedelvärdet(tal1, tal2, tal3);
+}
+
+function räknaUtMedelvärdet(tal1,tal2,tal3) {
+    return (Number(tal1) + Number(tal2) + Number(tal3)) / 3;
+}
+
+function summan() {
+    var tal1 = document.getElementById('tal1').value;
+    var tal2 = document.getElementById('tal2').value;
+    var tal3 = document.getElementById('tal3').value;
+    document.getElementById('summa').innerHTML = addera(tal1, tal2, tal3);
+}
+
+function addera(tal1,tal2,tal3) {
+    return Number(tal1) + Number(tal2) + Number(tal3);
+}
