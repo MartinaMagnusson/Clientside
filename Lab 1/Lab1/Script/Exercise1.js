6a41cedb4b7c61f83b012fcafd0e25b18869aa00
Martina Magnusson <magnusson.martina@hotmail.com>
2015-12-21 17:24:31 +0100

    Skapat happy hour och dagens erbjudande

---
+++ Lab 1/Lab1/Script/Exercise1.js
@@ -0,0 +1,14 @@
+﻿var lön = document.getElementById("lön").addEventListener("click", bruttoLön, false);
+var brutto = document.getElementById('brutto').innerHTML;
+
+function bruttoLön() {
+    var försäljning = document.getElementById("försäljningssumman").value;
+    if(isNaN(försäljning))
+        document.getElementById("lön").innerHTML = Calculate(brutto, försäljning);
+    else
+        document.getElementById("lön").innerHTML = "Skriv in ett tal";
+}
+
+function Calculate(brutto, försäljning) {
+    return Number(brutto) + (Number(försäljning) * 0.09);
+}
---
+++ Lab 1/Lab1/Script/Exercise3.js
@@ -0,0 +1,14 @@
+﻿var summa = document.getElementById('summa2').addEventListener('click', skrivUtSumman, false);
+
+function skrivUtSumman() {
+    var tal = document.getElementById('tal').value;
+    if (isNaN(tal)) 
+        document.getElementById('summa2').innerHTML = räknaUtSumman(tal);
+    else
+        document.getElementById('summa2').innerHTML = "Skriv in ett nummer";
+        
+}
+
+function räknaUtSumman(tal) {
+    return (Number(tal) * 3) + 7;
+}
