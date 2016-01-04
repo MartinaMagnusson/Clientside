f39d4f25afbb4e162b16c6178519f62d3993e044
Martina Magnusson <magnusson.martina@hotmail.com>
2016-01-04 12:38:58 +0100

    lekt med metoder och properties

--- Lab 2/Exercise2/FixItPage.js
+++ Lab 2/Exercise2/FixItPage.js
@@ -1,35 +1,163 @@
-﻿/*
+﻿
+/*
 
 For each of the following aplicable exercise below you should present a box on the page 
 with an alert that shows the result when the box is clicked!
 
 ###Window object at page 124:###
 1. Link the script with FixIt.hmtl
-2. Add som tags that doesn't change the visual part of the paragraph.
+2. Add som tags that doesn't change the visual part of the paragraph.  - kan strykas!!!
+
 3.Play with the properties at page 124 in the book. 
   Use properties creatively to display things at the html page
-4.Add a stylesheet that displays the changed words in fat font and in red. - kan strykas!!!
-5. PLay with the Methods 
-### DOM page 126:###
-6. Play with the DOM-properties
+  */
 
-### String objects page 128, 129: ###
+function P(name, windowObject) {
+    this.name = name;
+    this.windowObject = windowObject;
+}
 
-7. Save the the text from the makeMeAnArray-paragraph into an array.
-8. Use all the string methods and propertys allong with the array
+var p;
+var span;
+var body = document.getElementsByTagName('body');
 
-### String objects page 132: ###
-9. check if the 4th element in the array is a number
+var listOfP = [
+    new P("inner height: ", window.innerHeight),
+    new P("inner widthr: ", window.innerWidth),
+    new P("page X offset: ", window.pageXOffset),
+    new P("page Y offset: ", window.pageYOffset),
+    new P("screen X: ", window.screenX),
+    new P("screen Y: ", window.screenY),
+    new P("location: ", window.location),
+    new P("document: ", window.document),
+    new P("history: ", window.history),
+    new P("history length: ", window.history.length),
+    new P("screen: ", window.screen),
+    new P("screen width: ", window.screen.width),
+    new P("screen height: ", window.screen.height),
+    new P("alert: ", 'click here'),
+    new P("open: ", 'click here'),
+    new P("print: ", 'click here'),
+    new P("last Modified: ", 'click here'),
+    new P("URL: ", 'click here'),
+    new P("domain: ", 'click here')
+];
 
-### Math page 134: ###
-10. Round one of the numbers in the paragraph up/down
-11. replace the 3rd word with PI then roud it to the nearest integer
+//4.Add a stylesheet that displays the changed words in fat font and in red.
+for (var i = 0; i < listOfP.length; i++) {
+    p = document.createElement('p');
+    span = document.createElement('span');
+    span.style.color = 'red';
+    span.style.fontWeight = 'bold';
 
-### Date object###
-12. Calculate how many days it's until your birthday and present it.
-13. Calculate how many minutes old you are and present it.
+    if (listOfP[i].name === 'alert: ')
+        p.setAttribute('id', 'alertThis');
+    if (listOfP[i].name === 'open: ')
+        p.setAttribute('id', 'openThis');
+    if (listOfP[i].name === 'print: ')
+        p.setAttribute('id', 'printThis');
 
-Bonus exercises:
+    p.textContent = listOfP[i].name;
+    span.textContent = listOfP[i].windowObject;
+    body[0].appendChild(p);
+    p.appendChild(span);
+}
+
+//5. PLay with the Methods 
+var alertThis = document.getElementById('alertThis');
+var openThis = document.getElementById('openThis');
+var printThis = document.getElementById('printThis');
+
+alertThis.addEventListener('click', function (e) {
+    alert("Hello World");
+    e.stopPropagation();
+}, true);
+
+openThis.addEventListener('click', function (e) {
+    open("http://facebook.com");
+    e.stopPropagation();
+}, true);
+
+printThis.addEventListener('click', function (e) {
+    print(body);
+    e.stopPropagation();
+}, true);
+
+/*### DOM page 126:###
+6. Play with the DOM-properties*/
+document.title = 'Menu';
+alert("lastModified: " + document.lastModified + '<br>');
+alert("URL: " + (document.url = 'Lab2'));
+alert("domain: " + document.domain);
+
+/*### String objects page 128, 129: ###
+7. Save the the text from the makeMeAnArray-paragraph into an array.*/
+var makeMeAnArray = "We have burgers, subs, pizza, and drinks";
+var myArray = makeMeAnArray.split(' ');
+//8. Use all the string methods and propertys allong with the array
+console.log('length: ' + makeMeAnArray.length);
+console.log('toUpperCase(): ' + makeMeAnArray.toUpperCase());
+console.log('toLocaleLowerCase(): ' + makeMeAnArray.toLocaleLowerCase());
+console.log('charAt(12): ' + makeMeAnArray.charAt(12));
+console.log('indexOf("zz"): ' + makeMeAnArray.indexOf('zz'));
+console.log('lastIndexOf("a"): ' + makeMeAnArray.lastIndexOf('a'));
+console.log('substring(8, 15): ' + makeMeAnArray.substring(8, 15));
+console.log('split(" "): ' +  makeMeAnArray.split(' '));
+console.log('trim(): ' + makeMeAnArray.trim());
+console.log('replace("s", "king"): ' + makeMeAnArray.replace('s', 'king'));
+
+/*### String objects page 132: ###
+9. check if the 4th element in the array is a number*/
+if (isNaN(makeMeAnArray[4]))
+    console.log("4th element in the array is not a number");
+else
+    console.log("4th element in the array is a number");
+
+/*### Math page 134: ###
+10. Round one of the numbers in the paragraph up/down*/
+var originalNumber = '102.23456';
+console.log(originalNumber);
+console.log(Math.ceil(originalNumber));
+console.log(Math.floor(originalNumber));
+
+//11. replace the 3rd word with PI then roud it to the nearest integer
+myArray[2] = Math.PI;
+myArray[2] = Math.round(myArray[2]);
+console.log(myArray);
+
+/*### Date object###
+12. Calculate how many days it's until your birthday and present it.*/
+var oneMinute = 60 * 1000;
+var oneHour = oneMinute * 60;
+var oneDay = oneHour * 24;
+var today = new Date();
+var oneYesr = oneDay * 365;
+
+function getDaysUntilMyBirthday() {
+    var birthday = new Date();
+    birthday.setMonth(10);
+    birthday.setDate(17);
+    var days;
+    if (today.getMonth() === 10 && today.getDate() === 17)
+        days = (today.getFullYear() + 1);
+    else
+        days = Math.round(Math.abs((today.getTime() - birthday.getTime()) / (oneDay)));
+    console.log("it is " + days + " days to my birthday");
+}
+getDaysUntilMyBirthday();
+
+//13. Calculate how many minutes old you are and present it.
+function getAge(dateString) {
+    var birthDate = new Date(dateString);
+    var minnutes = ((today.getFullYear() - birthDate.getFullYear()) * oneYesr)/ 1000;
+    var month = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+        minnutes--;
+    }
+    return minnutes;
+}
+console.log('minnutes old: ' + getAge("1992/11/17"));
+/*Bonus exercises:
 ### Demo page 141:###
 -Download the code to the book. Wipe the code from the js-file and rewrite it with the help from the book.
 -For additional reading and training go to: http://www.w3schools.com/jsref/jsref_obj_string.asp
--- Lab 2/Exercise2/MenuPage2.js
+++ Lab 2/Exercise2/MenuPage2.js
@@ -4,16 +4,24 @@
    Please use the same html syntax of an article as in exercise1.
 */
 
-var burgersPlace = document.getElementById('burgersPlace');
-var subsPlace = document.getElementById('subsPlace');
-var pizzaPlace = document.getElementById('pizzaPlace');
-var drinksPlace = document.getElementById('drinksPlace');
+var burgers = document.getElementById('burgersPlace');
+var subs = document.getElementById('subsPlace');
+var pizza = document.getElementById('pizzaPlace');
+var drinks = document.getElementById('drinksPlace');
 
 var h1 = document.createElement('h1');
 h1.textContent = "Menu";
 h1.setAttribute('id', 'ad');
+
 var body = document.getElementsByTagName('body');
-body[0].insertBefore(h1, burgersPlace);
+var div = document.getElementsByTagName('div');
+body[0].insertBefore(h1, burgers);
+
+var productTypes = ['Burgers', 'Subs', 'Pizza', 'Drinks'];
+
+for (var i = 0; i < productTypes.length; i++) {
+    createProductHeadding(productTypes[i]);
+}
 
 var listOfProducts = [
     new Product("Pepper burger", 90, "Monday", 19.90),
@@ -52,54 +60,70 @@ for (var i = 0; i < listOfProducts.length; i++) {
     addProductToElement(listOfProducts[i]);
 }
 
-function addProductToElement(element) {
+function createProductHeadding(productType) {
     var h2 = document.createElement('h2');
+    h2.textContent = 'Delicious ' + productType;
+    if (productType === 'Burgers')
+        div[0].appendChild(h2);
+    if (productType === 'Subs')
+        div[1].appendChild(h2);
+    if (productType === 'Pizza')
+        div[2].appendChild(h2);
+    if (productType === 'Drinks')
+        div[3].appendChild(h2);
+}
+
+function addProductToElement(element) {
     var article = document.createElement('article');
     var fieldset = document.createElement('fieldset');
     var legend = document.createElement('legend');
     var h3 = document.createElement('h3');
     var span = document.createElement('span');
+    var image = document.createElement('img');
     var div = document.createElement('div');
 
-    setAttributeOnProductElement(element, h1, h2, article, fieldset, legend, h3, span, div);
+    setAttributeOnProductElement(element, article, fieldset, legend, h3, span, image, div);
 }
 
-function setAttributeOnProductElement(element, h1, h2, article, fieldset, legend, h3, span, div) {
+function setAttributeOnProductElement(element, article, fieldset, legend, h3, span, image, div) {
+    article.style.display = 'inline-block';
+    article.style.backgroundColor = 'transparent';
+    fieldset.style.width = '230px';
     h3.innerHTML = element.name;
-    span.innerHTML = "No image loaded";
     div.innerHTML = element.price;
-    div.setAttribute('class', 'price');
+    div.setAttribute('class', 'price ' + element.dayOfTheWeek);
     switch (true) {
         case element.name.endsWith('burger'):
             legend.innerHTML = element.dayOfTheWeek + ' Burger';
-            div.setAttribute('id', element.dayOfTheWeek + 'Burger');
-            createHamburgerElement(article, fieldset, legend, h3, span, div);
+            image.src = 'Images/Hamburger.png';
+            createHamburgerElement(article, fieldset, legend, h3, span, image, div);
             break;
         case element.name.endsWith('drink'):
             legend.innerHTML = element.dayOfTheWeek + ' Drink';
-            div.setAttribute('id', element.dayOfTheWeek + 'Drink');
-            createDrinkElement(article, fieldset, legend, h3, span, div);
+            image.src = 'Images/Drink.png';
+            createDrinkElement(article, fieldset, legend, h3, span, image, div);
             break;
         case element.name.endsWith('pizza'):
             legend.innerHTML = element.dayOfTheWeek + ' Pizza';
-            div.setAttribute('id', element.dayOfTheWeek + 'Pizza');
-            createPizzaElement(article, fieldset, legend, h3, span, div);
+            image.src = 'Images/Pizza.png';
+            createPizzaElement(article, fieldset, legend, h3, span, image, div);
             break;
         default:
             legend.innerHTML = element.dayOfTheWeek + ' Sub';
-            div.setAttribute('id', element.dayOfTheWeek + 'Sub');
-            createSubElement(article, fieldset, legend, h3, span, div);
+            image.src = 'Images/Subs.png';
+            createSubElement(article, fieldset, legend, h3, span, image, div);
             break;
     }
 }
 
-function createHamburgerElement(article, fieldset, legend, h3, span, div) {
+function createHamburgerElement(article, fieldset, legend, h3, span, image, div) {
     fieldset.appendChild(legend);
     fieldset.appendChild(h3);
     fieldset.appendChild(span);
     fieldset.appendChild(div);
+    span.appendChild(image);
     article.appendChild(fieldset);
-    burgersPlace.appendChild(article);
+    burgers.appendChild(article);
 }
 
 /*
@@ -113,33 +137,95 @@ function Product(name, size, dayOfTheWeek, price) {
     this.price = price;
 }
 //3. add 7 drinks to the page using Product.
-function createDrinkElement(article, fieldset, legend, h3, span, div) {
+function createDrinkElement(article, fieldset, legend, h3, span, image, div) {
     fieldset.appendChild(legend);
     fieldset.appendChild(h3);
     fieldset.appendChild(span);
     fieldset.appendChild(div);
+    span.appendChild(image);
     article.appendChild(fieldset);
-    drinksPlace.appendChild(article);
+    drinks.appendChild(article);
 }
 //4. add 7 pizzas to the page using Product.
-function createPizzaElement(article, fieldset, legend, h3, span, div) {
+function createPizzaElement(article, fieldset, legend, h3, span, image, div) {
     fieldset.appendChild(legend);
     fieldset.appendChild(h3);
     fieldset.appendChild(span);
     fieldset.appendChild(div);
+    span.appendChild(image);
     article.appendChild(fieldset);
-    pizzaPlace.appendChild(article);
+    pizza.appendChild(article);
 }
 //5. add 7 subs to the page using Product.
-function createSubElement(article, fieldset, legend, h3, span, div) {
+function createSubElement(article, fieldset, legend, h3, span, image, div) {
     fieldset.appendChild(legend);
     fieldset.appendChild(h3);
     fieldset.appendChild(span);
     fieldset.appendChild(div);
+    span.appendChild(image);
     article.appendChild(fieldset);
-    subsPlace.appendChild(article);
+    subs.appendChild(article);
 }
 //6. GO to file FixIt.html
 
 //DRINKS
 //Subs
+
+//happy hour logic:
+var date = new Date();
+var price = document.getElementsByClassName('price');
+
+function happyHour() {
+    if (date.getHours() === 21) {
+        for (var i = 0; i < price.length; i++) {
+            price[i].innerHTML = GetPrice(price[i].innerHTML);
+        }
+    }
+}
+//Method calls:
+happyHour();
+
+//Adjust price function: (add .toFixed(2) for 2 decimals)
+function GetPrice(price) {
+    return (price * 0.75).toFixed(2);
+}
+
+//todays offer function.
+switch (true) {
+    case (date.getDay() === 0):
+        var sunday = document.getElementsByClassName('Sunday');
+        todaysOffer(sunday);
+        break;
+    case (date.getDay() === 1):
+        var monday = document.getElementsByClassName('Monday');
+        todaysOffer(monday);
+        break;
+    case (date.getDay() === 2):
+        var tuesday = document.getElementsByClassName('Tuseday');
+        todaysOffer(tuesday);
+        break;
+    case (date.getDay() === 3):
+        var wednesday = document.getElementsByClassName('Wednesday');
+        todaysOffer(wednesday);
+        break;
+    case (date.getDay() === 4):
+        var thursday = document.getElementsByClassName('Thursday');
+        todaysOffer(thursday);
+        break;
+    case (date.getDay() === 5):
+        var friday = document.getElementsByClassName('Friday');
+        todaysOffer(friday);
+        break;
+    case (date.getDay() === 6):
+        var saturday = document.getElementsByClassName('Saturday');
+        todaysOffer(saturday);
+        break;
+    default:
+}
+
+function todaysOffer(element) {
+    for (var i = 0; i < element.length; i++) {
+        element[i].style.backgroundColor = 'red';
+        element[i].innerHTML = (element[i].innerHTML * 0.80);
+    }
+}
