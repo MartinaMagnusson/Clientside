782c1ae69dc4d24746f0a1c84e7d5b32ad03ba78
Martina Magnusson <magnusson.martina@hotmail.com>
2016-01-06 10:59:01 +0100

    fixat så att det inte står undefined överst bland produkterna när man söker

--- Lab 2/SearchIntegration/Scripts/Index.js
+++ Lab 2/SearchIntegration/Scripts/Index.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
+    var output;
     $.getJSON('product.json', function (data) {
-        var output = "";
+        output = "";
         $.each(data.product, function (key, val) {
             output += " <h2 class='text-center'>" + val.productType + " </h2>";
             $.each(val.products, function (productKey, productVal) {
@@ -9,7 +10,7 @@
                 output += "<div class='col-sm-8'>";
                 output += "<h3>" + productVal.productName + "</h3>";
                 output += "<p>" + productVal.description + "</p>";
-                output += "<p>" + productVal.price + "kr</p>";
+                output += "<p class='price'>" + productVal.price + "kr</p>";
                 output += "</div>";
                 output += "<div class='col-sm-4'>";
                 output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
@@ -19,14 +20,14 @@
             $('#menuPlace').html(output);
         });
     });
-    var output;
-    $('button').on('click', function () {
 
+    $('button').on('click', function () {
+        output = "";
         var productType = $(this).text();
         filterByCategory(productType, output, null);
     });
     $('#search').keyup(function () {
-
+        output = "";
         var searchField = $(this).val();
         var regex = new RegExp(searchField, "i");
         searchByText(regex, output);
@@ -37,16 +38,16 @@ function filterByCategory(type, output, regex) {
     $.getJSON('product.json', function (data) {
         $.each(data.product, function (key, val) {
             if (val.productType === type) {
-                if ((regex !== "") || (regex !== null)) {
+                if ((regex !== "") && (regex !== null)) {
                     output += " <h2 class='text-center'>" + val.productType + " </h2>";
                     $.each(val.products, function (productKey, productVal) {
-                        if ((val.productType.search(regex) !== -1) || (productVal.productName.search(regex) !== -1)) {
+                        if ((productVal.description.search(regex) !== -1) || (productVal.productName.search(regex) !== -1)) {
                             output += "<article class='col-sm-12'><fieldset>";
                             output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
                             output += "<div class='col-sm-8'>";
                             output += "<h3>" + productVal.productName + "</h3>";
                             output += "<p>" + productVal.description + "</p>";
-                            output += "<p>" + productVal.price + "kr</p>";
+                            output += "<p class='price'>" + productVal.price + "kr</p>";
                             output += "</div>";
                             output += "<div class='col-sm-4'>";
                             output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
@@ -63,7 +64,7 @@ function filterByCategory(type, output, regex) {
                         output += "<div class='col-sm-8'>";
                         output += "<h3>" + productVal.productName + "</h3>";
                         output += "<p>" + productVal.description + "</p>";
-                        output += "<p>" + productVal.price + "kr</p>";
+                        output += "<p class='price'>" + productVal.price + "kr</p>";
                         output += "</div>";
                         output += "<div class='col-sm-4'>";
                         output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
@@ -71,39 +72,41 @@ function filterByCategory(type, output, regex) {
                         output += "</article></fieldset>";
                     });
                 }
+                if (output != null || output !== "")
+                    $('#menuPlace').html(output);
             }
         });
-        $('#menuPlace').html(output);
     });
 }
 
 function searchByText(regex, output) {
-        $.getJSON('product.json', function (data) {
-            $.each(data.product, function (key, val) {
-                if (val.productType.search(regex) !== -1)
-                    output += " <h2 class='text-center'>" + val.productType + " </h2>";
-                $.each(val.products, function (productKey, productVal) {
-                    if ((val.productType.search(regex) !== -1) || (productVal.productName.search(regex) !== -1)) {
-                        output += "<article class='col-sm-12'><fieldset>";
-                        output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
-                        output += "<div class='col-sm-8'>";
-                        output += "<h3>" + productVal.productName + "</h3>";
-                        output += "<p>" + productVal.description + "</p>";
-                        output += "<p>" + productVal.price + "kr</p>";
-                        output += "</div>";
-                        output += "<div class='col-sm-4'>";
-                        output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
-                        output += "</div>";
-                        output += "</article></fieldset>";
-                    }
-                });
-                $('#menuPlace').html(output);
+    $.getJSON('product.json', function (data) {
+        $.each(data.product, function (key, val) {
+            if (val.productType.search(regex) !== -1)
+                output += " <h2 class='text-center'>" + val.productType + " </h2>";
+            $.each(val.products, function (productKey, productVal) {
+                if ((productVal.description.search(regex) !== -1) || (productVal.productName.search(regex) !== -1)) {
+                    output += "<article class='col-sm-12'><fieldset>";
+                    output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
+                    output += "<div class='col-sm-8'>";
+                    output += "<h3>" + productVal.productName + "</h3>";
+                    output += "<p>" + productVal.description + "</p>";
+                    output += "<p class='price'>" + productVal.price + "kr</p>";
+                    output += "</div>";
+                    output += "<div class='col-sm-4'>";
+                    output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
+                    output += "</div>";
+                    output += "</article></fieldset>";
+                }
             });
-            if ($('button').click) {
-                $('button').on('click', function() {
-                    var productType = $(this).text();
-                    filterByCategory(productType, output = "", regex);
-                });
-            }
-        });   
+            if (output != null || output !== "")
+                $('#menuPlace').html(output);
+        });
+        if ($('button').click) {
+            $('button').on('click', function () {
+                var productType = $(this).text();
+                filterByCategory(productType, output = "", regex);
+            });
+        }
+    });
 }
