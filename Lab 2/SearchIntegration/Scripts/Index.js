ca84774f8b47bd7f1dcca508810695e90051aa66
Martina Magnusson <magnusson.martina@hotmail.com>
2016-01-07 18:59:19 +0100

    kan filtrera med både checkboxar och med text

--- Lab 2/SearchIntegration/Scripts/Index.js
+++ Lab 2/SearchIntegration/Scripts/Index.js
@@ -4,20 +4,21 @@
         createProducts();
 
     $('.category').on('click', function () {
+        var searchField = $('#search').val();
+        var regex = new RegExp(searchField, "i");
         $('h2').remove();
         $('article').remove();
         createProducts();
-        filterByCategory();
+        searchProduct(regex);
     });
 
     $('#search').keyup(function () {
+        var searchField = $(this).val();
+        var regex = new RegExp(searchField, "i");
         $('h2').remove();
         $('article').remove();
         createProducts();
-        //output = "";
-        var searchField = $(this).val();
-        var regex = new RegExp(searchField, "i");
-        searchByText(regex);
+        searchProduct(regex);
     });
 });
 
@@ -25,63 +26,38 @@ function createProducts() {
     $.getJSON('product.json', function (data) {
         var output = "";
         $.each(data.product, function (key, val) {
-            output += " <h2 class='text-center " + val.productType + "'>" + val.productType + " </h2>";
-            $.each(val.products, function (productKey, productVal) {
-                output += "<article class='col-sm-12 " + val.productType + "'><fieldset>";
-                output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
-                output += "<div class='col-sm-8'>";
-                output += "<h3>" + productVal.productName + "</h3>";
-                output += "<p>" + productVal.description + "</p>";
-                output += "<p class='price'>" + productVal.price + "kr</p>";
-                output += "</div>";
-                output += "<div class='col-sm-4'>";
-                output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
-                output += "</div>";
-                output += "</fieldset></article>";
-            });
+            if ($('#' + val.productType.toLocaleLowerCase()).is(":checked")) {
+                output += " <h2 class='text-center " + val.productType + "'>" + val.productType + " </h2>";
+                $.each(val.products, function (productKey, productVal) {
+                    output += "<article class='col-sm-12 " + val.productType
+                        + "' id='" + productVal.productName.replace(" ", "") + "'><fieldset>";
+                    output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
+                    output += "<div class='col-sm-8'>";
+                    output += "<h3>" + productVal.productName + "</h3>";
+                    output += "<p>" + productVal.description + "</p>";
+                    output += "<p class='price'>" + productVal.price + "kr</p>";
+                    output += "</div>";
+                    output += "<div class='col-sm-4'>";
+                    output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
+                    output += "</div>";
+                    output += "</fieldset></article>";
+                });
+            }
             $('#menuPlace').html(output);
         });
     });
 }
 
-function filterByCategory() {
-    $.getJSON('product.json', function (data) {
-        $.each(data.product, function (key, val) {
-            if (!$('#' + val.productType.toLocaleLowerCase()).is(":checked"))
-                $('.' + val.productType).hide();
-        });
-    });
-}
-
-function searchByText(regex) {
+function searchProduct(regex) {
     $.getJSON('product.json', function (data) {
         $.each(data.product, function (key, val) {
             $.each(val.products, function (productKey, productVal) {
-                if ((productVal.description.search(regex) !== -1) || (productVal.produccontainstName.search(regex) !== -1)) {
-                    $(val).hide();
-                    //output += "<article class='col-sm-12'><fieldset>";
-                    //output += " <legend>" + productVal.day + " " + val.productType + " </legend>";
-                    //output += "<div class='col-sm-8'>";
-                    //output += "<h3>" + productVal.productName + "</h3>";
-                    //output += "<p>" + productVal.description + "</p>";
-                    //output += "<p class='price'>" + productVal.price + "kr</p>";
-                    //output += "</div>";
-                    //output += "<div class='col-sm-4'>";
-                    //output += '<img src="' + productVal.image + '" alt="' + productVal.description + '"/>';
-                    //output += "</div>";
-                    //output += "</article></fieldset>";
+                if ((productVal.description.search(regex) === -1) && (productVal.productName.search(regex) === -1)) {
+                    $('#' + productVal.productName.replace(" ", "")).hide();
                 }
-                //if (hideProducttype === true)
-                //    $('h2').hide();
             });
-            //if (output != null || output !== "")
-            //    $('#menuPlace').html(output);
+            if (!$('#' + val.productType.toLocaleLowerCase()).is(":checked"))
+                $('.' + val.productType).hide();
         });
-        //if ($('button').click) {
-        //    $('button').on('click', function () {
-        //        var productType = $(this).text();
-        //        filterByCategory(productType, output = "", regex);
-        //    });
-        //}
     });
 }
